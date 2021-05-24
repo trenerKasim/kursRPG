f312f761dd954392aa14c44b8e5c95c1caf7b698
trenerKasim <jasinski9948@gmail.com>
2021-05-24 18:57:51 +0200

    second and third part of first section

---
+++ rpgKurs/Assets/Mover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+	[SerializeField]
+	private Transform target;
+	public Transform Target
+	{
+		get { return target; }
+		set { target = value; }
+	}
+	
+    // Update is called once per frame
+    void Update()
+    {
+		GetComponent<NavMeshAgent>().destination = target.position;
+    }
+}
